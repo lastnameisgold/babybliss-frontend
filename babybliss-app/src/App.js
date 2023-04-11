c37aef3f7ea21df22bbaafafc38c8a148abd59e9
Fernando <me@fdorado.com>
2023-04-11 19:08:57 -0400

    Created react app.

---
+++ babybliss-app/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Home } from "./components/Home";
+import { Navigation } from "./components/Navigation";
+import { Logout } from "./components/Logout";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
