b6f38b8ac905e80c847e68160b9b4e6a4ab3daed
Fernando <me@fdorado.com>
2023-04-16 20:40:56 -0400

    Fixed styling for AddDiaper component.

--- babybliss-app/src/components/AddDiaper.jsx
+++ babybliss-app/src/components/AddDiaper.jsx
@@ -54,55 +54,76 @@ export default function AddDiaper(props) {
   };
 
   return (
-    <div className="p-4">
-      <form>
-        {showAlert && <div className="alert alert-success">Diaper logged</div>}
-        <div className="form-group p-4">
-          <label htmlFor="log">Date and time</label>
-          <input
-            type="datetime-local"
-            className="form-control"
-            id="log"
-            name="log"
-            value={formData.log}
-            onChange={handleChange}
-          />
+    <div className="form-container p-4 d-flex justify-content-center align-items-center">
+      <form className="w-50">
+        <div className="form-group p-4 d-grid gap-3">
+          {showAlert && (
+            <div className="alert alert-success">Diaper logged</div>
+          )}
 
-          <label htmlFor="diaper">Diaper</label>
-          <select
-            className="form-control"
-            id="diaper"
-            name="diaper"
-            value={formData.diaper}
-            onChange={handleChange}
-          >
-            <option value="">-- Select an option --</option>
-            <option value="1">Wet</option>
-            <option value="2">Dirty</option>
-          </select>
+          <h3>Add diaper</h3>
 
-          <label htmlFor="rash">Rash</label>
-          <select
-            className="form-control"
-            id="rash"
-            name="rash"
-            value={formData.rash}
-            onChange={handleChange}
-          >
-            <option value="">-- Select an option --</option>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
+          <div>
+            <label htmlFor="log" className="mb-1">
+              <small className="fw-semibold text-muted">Date and time</small>
+            </label>
+            <input
+              type="datetime-local"
+              className="form-control"
+              id="log"
+              name="log"
+              value={formData.log}
+              onChange={handleChange}
+            />
+          </div>
 
-          <label htmlFor="notes">Notes</label>
-          <input
-            type="text"
-            className="form-control"
-            id="notes"
-            name="notes"
-            value={formData.notes}
-            onChange={handleChange}
-          />
+          <div>
+            <label htmlFor="diaper" className="mb-1">
+              <small className="fw-semibold text-muted">Diaper</small>
+            </label>
+            <select
+              className="form-control"
+              id="diaper"
+              name="diaper"
+              value={formData.diaper}
+              onChange={handleChange}
+            >
+              <option value="">-- Select an option --</option>
+              <option value="1">Wet</option>
+              <option value="2">Dirty</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="rash" className="mb-1">
+              <small className="fw-semibold text-muted">Rash</small>
+            </label>
+            <select
+              className="form-control"
+              id="rash"
+              name="rash"
+              value={formData.rash}
+              onChange={handleChange}
+            >
+              <option value="">-- Select an option --</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="notes" className="mb-1">
+              <small className=" fw-semibold text-muted">Notes</small>
+            </label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="notes"
+              name="notes"
+              value={formData.notes}
+              onChange={handleChange}
+            />
+          </div>
 
           <button
             type="submit"
